7a6b0373cc255849091e796869b1aa5e09e501fe
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2024-09-30 15:02:30 -0400

    last

--- apps/extras/forms.py
+++ apps/extras/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 from .models import *
 
+class PaginaForm(forms.ModelForm):
+	class Meta:
+		model = Pagina
+		exclude = ['']
---
+++ apps/users/templatetags/gloval_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.apps import apps
+
+register = template.Library()
+
+@register.filter
+def verbose_name(obj):
+	return obj.model._meta.verbose_name if getattr(obj, 'model', False) else obj.form_class._meta.model._meta.verbose_name
+
+@register.filter
+def verbose_name_plural(obj):
+	return obj.model._meta.verbose_name_plural if getattr(obj, 'model', False) else obj.form_class._meta.model._meta.verbose_name_plural
+
+@register.simple_tag
+def get_model_verbose_name(app, model):
+	return apps.get_model(app, model)._meta.verbose_name.title()
+
+@register.simple_tag
+def get_model_verbose_name_plural(app, model):
+	return apps.get_model(app, model)._meta.verbose_name_plural.title()

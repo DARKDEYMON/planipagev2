82bc564cd7dd0d1f51f80074aa33688e81363885
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-10-02 10:17:29 -0400

    actualizacion miembros

--- apps/extras/urls.py
+++ apps/extras/urls.py
@@ -18,11 +18,16 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required, permission_required
 from .views import *
 
+app_name = 'extras' 
+
 urlpatterns = [
     path('listpaginas/', permission_required('users.extras')(login_required(ListPaginaView.as_view())), name='list_paginas'),
     path('createpaginas/', permission_required('users.extras')(login_required(CreatePaginaView.as_view())), name='create_paginas'),
     path('updatepaginas/<int:pk>/', permission_required('users.extras')(login_required(UpdatePaginaView.as_view())), name='update_paginas'),
-    path('deletepagina/<int:pk>/', permission_required('users.users')(login_required(DeletePaginaView.as_view())), name='delete_pagina')
-
-
+    path('deletepagina/<int:pk>/', permission_required('users.users')(login_required(DeletePaginaView.as_view())), name='delete_pagina'),
+   #miebros
+    path('miembro/', permission_required('users.extras')(login_required(ListMiembroView.as_view())), name='miembro_list'),
+    path('miembro/create/', permission_required('users.extras')(login_required(CreateMiembroView.as_view())), name='miembro_create'),
+    path('miembro/update/<int:pk>/', permission_required('users.extras')(login_required(UpdateMiembroView.as_view())), name='miembro_update'),
+    path('miembro/delete/<int:pk>/', permission_required('users.extras')(login_required(DeleteMiembroView.as_view())), name='miembro_delete'),
 ]

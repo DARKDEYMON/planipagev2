2fa40a08b5b08c7361ff2b3085974fc9dea90e2c
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2024-11-04 14:50:05 -0400

    e13

--- apps/tipos/middleware.py
+++ apps/tipos/middleware.py
@@ -1,9 +1,12 @@
 from .models import *
+from django.db.models import Prefetch, Q
 
 class TiposMaestrosMiddleware:
 	def __init__(self, get_response):
 		self.get_response = get_response
 	def __call__(self, request):
-		request.tipo_maestro = TipoMaestro.objects.all().prefetch_related('tipo_set')
+		request.tipo_maestro = TipoMaestro.objects.all().prefetch_related(
+			Prefetch('tipo_set', Tipo.objects.order_by('prioridad','id')),
+		).order_by('prioridad','id')
 		response = self.get_response(request)
 		return response

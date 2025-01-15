c7fa1ec8182cd70b6458f4023cf862f204324d02
Reynaldo Juan Pereira Heredia <darkdeymon04@gmail.com>
2025-01-15 17:19:58 -0400

    correcion ultimas noticias

--- apps/publicaciones/middleware.py
+++ apps/publicaciones/middleware.py
@@ -4,6 +4,6 @@ class UltimasPublicaionesMiddleware:
 	def __init__(self, get_response):
 		self.get_response = get_response
 	def __call__(self, request):
-		request.ultimas_publicaciones = Publicacion.objects.filter(publicado=True)[:5]
+		request.ultimas_publicaciones = Publicacion.objects.filter(publicado=True).order_by('-id')[:5]
 		response = self.get_response(request)
 		return response
